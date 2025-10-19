ea1e2f0c3db4022d8d12767d2c21033466a6853c
GodwinCyber <godwinchuks032@gmail.com>
2025-10-19 15:13:12 +0100

    Fixing the setting requirement

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -31,19 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crm',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # external libraries
     'graphene_django',
     'django_filters',
-
-    # local apps
-    'crm',
 ]
 
 MIDDLEWARE = [
